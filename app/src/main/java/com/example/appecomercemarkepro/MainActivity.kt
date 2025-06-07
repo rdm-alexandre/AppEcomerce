1074c54cf48d1a429058530266ddffa82d2161cc
Ruan de Moraes Alexandre <mruan309@gmail.com>
2025-06-06 22:08:16 -0300

    Inicio da listagem das categorias

--- app/src/main/java/com/example/appecomercemarkepro/MainActivity.kt
+++ app/src/main/java/com/example/appecomercemarkepro/MainActivity.kt
@@ -20,13 +20,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_favorite, R.id.navigation_cart, R.id.navigation_notification, R.id.navigation_user
+                R.id.navigation_home,
+                R.id.navigation_favorite,
+                R.id.navigation_cart,
+                R.id.navigation_notification,
+                R.id.navigation_user
             )
         )
+
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/appecomercemarkepro/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appecomercemarkepro/ui/home/HomeFragment.kt
@@ -4,27 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.example.appecomercemarkepro.R
 import com.example.appecomercemarkepro.adapters.ImageAdapter
 import com.example.appecomercemarkepro.data.model.ImageItem
-import com.example.appecomercemarkepro.databinding.FragmentHomeBinding
-import com.example.appecomercemarkepro.ui.favorite.FavoriteFragment
-import com.example.appecomercemarkepro.ui.favorite.FavoriteViewModel
 import java.util.UUID
 
 class HomeFragment : Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var pageChangeListener: OnPageChangeCallback
+
+    private val params = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    ).apply {
+        setMargins(8, 0, 8, 0)
+    }
 
     companion object {
         fun newInstance() = HomeFragment()
     }
-
-    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,38 +47,49 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val rvCarousel = view.findViewById<RecyclerView>(R.id.recyclerViewCarousel)
-        val imageAdapter = ImageAdapter()
-        rvCarousel.adapter = imageAdapter
+        viewPager2 = view.findViewById<ViewPager2>(R.id.viewPager2)
 
         val imageList = arrayListOf(
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/loucas/wordpress/prod/sites/7/2024/01/05092334/blog_a-beleza-do-seu-carnavalHUB_banner-header_1361x618-768x349.jpg"
             ),
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/loucas/wordpress/prod/sites/7/2023/03/10152007/%E2%80%A2-1361x618_23_Banner_Carnaval-1-768x349.jpg"
             ),
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://marcaspelomundo.com.br/wp-content/uploads/2023/02/1210006221.jpg"
             ),
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_OGSHR7mpwXhe4I893P4zSngHxQO83d6oTOKqmC923njruQ_PbfJZ9TVwEIR32FfP-2A&usqp=CAU"
             ),
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/blog/wordpress/prod/sites/7/2024/01/31092435/HUB_banner-blog1_1361x618.jpg"
             ),
             ImageItem(
                 UUID.randomUUID().toString(),
-                "https://i.pinimg.com/736x/c3/08/57/c308573a4f1c776630b05befbe5934d4.jpg"
+                "https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/loucas/wordpress/prod/sites/7/2025/02/05160817/blog_1361x618_25_ROTINA-Carnaval-768x349.jpg"
             )
         )
 
+        val imageAdapter = ImageAdapter()
+        viewPager2.adapter = imageAdapter
         imageAdapter.submitList(imageList)
+
+        pageChangeListener = object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                viewPager2.registerOnPageChangeCallback(pageChangeListener)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewPager2.unregisterOnPageChangeCallback(pageChangeListener)
     }
 
 }
